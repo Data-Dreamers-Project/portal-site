e0a5ad6e6223ab1484af6899f952224cca9704a3
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2026-03-24 19:26:07 +0900

    fix: replace deprecated next/image layout prop with fill pattern

--- src/libs/html-react-parser/options.tsx
+++ src/libs/html-react-parser/options.tsx
@@ -148,14 +148,29 @@ const replaceElements: Record<
 			</Box>
 		</blockquote>
 	),
-	img: ({ children, src, alt, ...props }) => {
+	img: ({ src, alt, width, height }) => {
+		// width/height が取れる場合はそちらを優先
+		if (width && height) {
+			return (
+				<Image
+					src={src}
+					alt={alt ?? ""}
+					width={Number(width)}
+					height={Number(height)}
+					style={{ maxWidth: "100%", height: "auto" }}
+				/>
+			);
+		}
+		// サイズ不明時は fill で対応
 		return (
-			<Image
-				src={src}
-				alt={alt}
-				layout="intrinsic" // アスペクト比を自動計算
-				{...props}
-			/>
+			<Box position="relative" w="full" minH="300px">
+				<Image
+					src={src}
+					alt={alt ?? ""}
+					fill
+					style={{ objectFit: "contain" }}
+				/>
+			</Box>
 		);
 	},
 };
